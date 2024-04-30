7509166e28adb00b55abef4fc2ecd9509d23cc6d
KhaHo <khahotravinh2002@gmail.com>
2024-04-30 23:53:32 +0700

    push code

--- src/app/clients/product-kid/product-kid.component.ts
+++ src/app/clients/product-kid/product-kid.component.ts
@@ -1,7 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Subject, from } from 'rxjs';
+import { partition } from 'rxjs/operators';
+import { FunctionAlert } from 'src/app/function_alert/function_alert';
+import { ApiCustomerService } from 'src/app/services/api_customer/api-customer.service';
 import { ApiProductsService } from 'src/app/services/api_products/api-product.service';
+import { addItemCart } from 'src/app/store/app.action';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-product-kid',
@@ -9,7 +14,7 @@ import { ApiProductsService } from 'src/app/services/api_products/api-product.se
   styleUrls: ['./product-kid.component.scss']
 })
 export class ProductKidComponent {
-  constructor(private productService: ApiProductsService) { }
+  constructor(private productService: ApiProductsService, private functionAlert: FunctionAlert, private apiCustomerService: ApiCustomerService, private store: Store) { }
 
 
 
@@ -24,8 +29,11 @@ export class ProductKidComponent {
   listSize: any[] = [];
   listGetSize: any[] = [];
   listGetColor: any[] = [];
+  listColorProduct: any[] = [];
   // listProductFilter: any[] = [];
+  getColor: string | any;
   dataSendApi: any[] = [];
+  buttonStates: boolean[] = [];
 
   @Input() childMessage: string | undefined;
 
@@ -38,20 +46,19 @@ export class ProductKidComponent {
   }
 
   getProductKid() {
-    this.productService.getAllProduct().subscribe((data:any) => {
+    this.productService.getAllProduct().subscribe((data: { errCode: number; data: any[]; }) => {
       if (data && data.errCode === 0) {
-        this.product_list = data.data;
-        this.product_list.forEach((element: any) => {
-          if (element.type_pro_sex == "kid") {
-            this.product_list_kid.push(element);
-          }
+        const [womenProducts$, otherProducts$] = partition((product: any) => product.type_pro_sex === 'kid')(from(data.data));
+        womenProducts$.subscribe((womenProduct: any) => {
+          this.product_list_kid.push(womenProduct);
+          this.listColorProduct = data.data[0].color;
         });
       }
-    })
+    });
   }
 
   getAllColor() {
-    this.productService.getAllColor().subscribe((data:any) => {
+    this.productService.getAllColor().subscribe((data: any) => {
       if (data && data.errCode === 0) {
         this.listColor = data.data;
       }
@@ -59,13 +66,37 @@ export class ProductKidComponent {
   }
 
   getAllSize() {
-    this.productService.getAllSize().subscribe((data:any) => {
+    this.productService.getAllSize().subscribe((data: any) => {
       if (data && data.errCode === 0) {
         this.listSize = data.data;
       }
     })
   }
 
+
+  getSizeValueFilter(size: any) {
+    size.isChecked = !size.isChecked;
+    if (size.isChecked) {
+      this.listGetSize.push(size.name_s);
+    } else {
+      const index = this.listGetSize.indexOf(size.name_s);
+      if (index !== -1) {
+        this.listGetSize.splice(index, 1);
+      }
+    }
+  }
+
+  getColorValueFilter(color: any) {
+    color.isChecked = !color.isChecked;
+    if (color.isChecked) {
+      this.listGetColor.push(color.code_color);
+    } else {
+      const index = this.listGetColor.indexOf(color.code_color);
+      if (index !== -1) {
+        this.listGetColor.splice(index, 1);
+      }
+    }
+  }
 
   getSizeValue(size: any) {
     size.isChecked = !size.isChecked;
@@ -79,16 +110,19 @@ export class ProductKidComponent {
     }
   }
 
-  
-  getColorValue(color: any) {
+
+  getColorValue(color: any, i: number) {
     color.isChecked = !color.isChecked;
     if (color.isChecked) {
-      this.listGetColor.push(color.code_color);
-    } else {
-      const index = this.listGetColor.indexOf(color.code_color);
-      if (index !== -1) {
-        this.listGetColor.splice(index, 1);
-      }
+      this.getColor = color;
+      this.product_list_kid[i].color.forEach((item: any) => {
+        if (item !== color) {
+          item.isChecked = false;
+        }
+      });
+    }
+    else {
+      this.getColor = '';
     }
   }
 
@@ -100,7 +134,7 @@ export class ProductKidComponent {
         color: this.listGetColor,
         type_sex: "kid"
       };
-      this.productService.filterProduct({ data: data }).subscribe((data:any) => {
+      this.productService.filterProduct({ data: data }).subscribe((data: any) => {
         if (data && data.errCode === 0) {
           this.product_list_kid = data.data;
         }
@@ -119,10 +153,59 @@ export class ProductKidComponent {
     }
   }
 
+
+  addCart(product: any, size: any) {
+    let tokenCustomer = sessionStorage.getItem('curentAccountCustomer');
+    if (tokenCustomer === null) {
+      this.functionAlert.showAlertAndNavigate("Bạn cần đăng nhập để thực hiện chức năng này !", '/login_customer');
+    }
+    else {
+      let checkTokenCustomer = this.apiCustomerService.checkTokenExpiration(tokenCustomer);
+      if (checkTokenCustomer == true) {
+        if (this.getColor !== undefined) {
+          this.store.dispatch(addItemCart({
+            item: {
+              idpro: product.idpro,
+              name_pro: product.name_pro,
+              image_pro: product.image_pro,
+              price: product.price,
+              sale: product.sale,
+              quantity: 1,
+              color: this.getColor.name_c,
+              size: size
+            }
+          }))
+        }
+        else {
+          this.store.dispatch(addItemCart({
+            item: {
+              idpro: product.idpro,
+              name_pro: product.name_pro,
+              image_pro: product.image_pro,
+              price: product.price,
+              sale: product.sale,
+              quantity: 1,
+              color: product.color[0].name_c,
+              size: size
+            }
+          }))
+        }
+      }
+      else {
+        this.functionAlert.showAlertAndNavigate("Phiên đăng nhập của bạn đã hết, vui lòng đăng  nhập lại !", '/login_customer');
+      }
+    }
+  }
+
   loadRouter() {
     location.replace("/clients/cart");
   }
 
+
+
+  toggleButtonState(index: number) {
+    this.buttonStates[index] = !this.buttonStates[index];
+  }
 
   pageChangeEvent(event: number) {
     this.p = event;
